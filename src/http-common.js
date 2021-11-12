a662fd9786214adb846d66f133ffe2c9cd649369
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-13 01:47:46 +0300

    api commit

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     //baseURL: "http://localhost:8080/api",
-    baseURL: "http://cors-anywhere-herokuapp.com/https://notesbackend-app.herokuapp.com/api",
+    baseURL: "https://cors-anywhere-herokuapp.com/https://notesbackend-app.herokuapp.com/api",
     headers: {
         "content-type": "application/json"
     }
