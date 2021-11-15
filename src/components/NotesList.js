db32984379791dd82fdb6d8789e4be80a3724d39
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-15 23:10:11 +0300

    some commit

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import NotesService from "../services/NotesService";
 import { Link } from "react-router-dom";
+import NotesService from "../services/NotesService";
 
 const NotesList = () => {
 
@@ -8,37 +8,32 @@ const NotesList = () => {
 
     useEffect(() => {
         NotesService.getAll()
-        .then(response => {
-
-            console.log('printing response', response.data);
-            setNotes(response.data);   
-        })
-        .catch(error => {
-            console.log('something went wrong', error);
-
-        })
+            .then(response => {
+                console.log('printing resposne', response.data);
+                setNotes(response.data);
+            })
+            .catch(error => {
+                console.log('something went wrong', error);
+            })
     }, []);
 
-
-    return ( 
+    return (  
         <div className="main-content">
             <h4>List of Notes</h4>
-          <div className="notes-list mt-4">
-               {
-                 notes.length > 0 ? notes.map(note => (
-                    <div key={note.id} className="notes-preview mt-3">
-                       <Link to={`/notes/${note.id}`}>
-                          <h5 className="primary-color text-capitalize">{note.title}</h5>
-                          <p>{note.body}</p>
-                       </Link>
-                    </div>
-
-                 )) : <div>No notes available</div>
-               }
-
-              </div>
+            <div className="notes-list mt-4">
+                {
+                    notes.length > 0 ? notes.map(note => (
+                        <div key={note.id} className="notes-preview mt-3">
+                            <Link to={`/notes/${note.id}`}>
+                                <h5 className="primary-color text-capitalize">{note.title}</h5>
+                                <p>{note.body}</p>
+                            </Link>
+                        </div>
+                    )) : <div>No notes available</div>
+                }
+            </div>
         </div>
-     );
+    );
 }
  
 export default NotesList;
