22e9bd9981ae3155ce32c50bd53d3f84b745b085
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-16 22:33:05 +0300

    completed delete feature

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
-    //baseURL: "https://notesbackend-app.herokuapp.com/api",
+    //baseURL: "http://localhost:8080/api",
+    baseURL: "https://notesbackend-app.herokuapp.com/api",
     headers: {
         "content-type": "application/json"
     }
